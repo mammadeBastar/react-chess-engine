5687e12c907bed129a99f143c5ab58791988885b
mammadeBastar <mamali2017basi@gmail.com>
2024-12-21 13:27:05 +0330

    working for now

--- src/helper.jsx
+++ src/helper.jsx
@@ -9,17 +9,17 @@ export const createPosition = () => {
         position[6][i] = 'bp'
     }
 
-     position[0][0] = position[0][7] = 'wr'
+    position[0][0] = position[0][7] = 'wr'
     position[0][1] = position[0][6] = 'wn'
     position[0][2] = position[0][5] = 'wb'
     position[0][3] = 'wq'
-     position[0][4] = 'wk'
+    position[0][4] = 'wk'
 
     position[7][0] = position[7][7] = 'br'
     position[7][1] = position[7][6] = 'bn'
     position[7][2] = position[7][5] = 'bb'
     position[7][3] = 'bq'
-     position[7][4] = 'bk'
+    position[7][4] = 'bk'
 
     return position
 
